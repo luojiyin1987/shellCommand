63d97b1653b30300415b51742eec4cf33557778e
luojiyin1987 <luojiyin@hotmail.com>
2020-03-17 15:36:03 +0800

    if no select file, eixt

--- cat.js
+++ cat.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 
 const args = process.argv.splice(2, process.argv.length-1);
 
+if(args.length <1)
+{
+    console.log("Please select file");
+    process.exit(1);
+}
+
 for(let i=0; i<args.length; i++){
     fs.readFile(args[i], (err,buf) => {
         console.log("=======> ", args[i]);
